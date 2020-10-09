4d9ab99fb773d814762a00c151a96e8e3005a710
Suraj-ui <sp990047@gmail.com>
2020-10-09 13:46:26 +0530

    Updates

---
+++ precache-manifest.e0fb69cb6934a6c5b7ac8db61a462d4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b390fc7553583d11beed12dca2bcde9",
+    "url": "/curdfire/index.html"
+  },
+  {
+    "revision": "157cb06c27c1299d2dfa",
+    "url": "/curdfire/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5ecac8352bb949516ae7",
+    "url": "/curdfire/static/css/main.76fbcb06.chunk.css"
+  },
+  {
+    "revision": "157cb06c27c1299d2dfa",
+    "url": "/curdfire/static/js/2.60d16842.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/curdfire/static/js/2.60d16842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ecac8352bb949516ae7",
+    "url": "/curdfire/static/js/main.59bc2a8f.chunk.js"
+  },
+  {
+    "revision": "caa6dfb6dc59e5927c45",
+    "url": "/curdfire/static/js/runtime-main.df6cdbf9.js"
+  }
+]);
